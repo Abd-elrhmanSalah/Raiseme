55211f3c96f9b6dd648fbdcbe14e49487d084fe8
Abdelrhman Salah <32471305+Abd-elrhmanSalah@users.noreply.github.com>
2025-11-17 10:55:00 +0200

    first commit

---
+++ src/test/java/com/eprogs/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eprogs.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
